19a44bb2aafb35ccf6b47b9a963c370977c9e3eb
guille-robotics <158527534+guille-robotics@users.noreply.github.com>
2026-04-08 09:18:42 -0400

    Improve error handling in calcular_eficiencia
    Added exception handling for assertion errors in calcular_eficiencia function.

--- Clases Practicas/Clase Practica 5/ejercicio_2.py
+++ Clases Practicas/Clase Practica 5/ejercicio_2.py
@@ -1,14 +1,16 @@
 def calcular_eficiencia(p_salida, p_entrada):
     # Aserción para validar datos lógicos
-    assert p_salida >= 0 and p_entrada >= 0, "Las potencias no pueden ser negativas"
-    
     try:
+        assert (p_salida >= 0 and p_entrada >= 0), "Las potencias no pueden ser negativas"
         eficiencia = (p_salida / p_entrada) * 100
         return f"Eficiencia: {eficiencia}%"
     except ZeroDivisionError:
         return "Error: Motor apagado o sin consumo (División por cero)."
+    except AssertionError as e:
+        return e
 
 # Pruebas
+print(calcular_eficiencia(-50, 100))
 print(calcular_eficiencia(150, 200))
 print(calcular_eficiencia(100, 0))
-#print(calcular_eficiencia(-50, 100)) # Descomentar para ver el AssertionError
+ 
